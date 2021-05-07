cea006c89f30c8499136abec9a3565f7ebca3d74
ncolonna <colonnan96@gmail.com>
2021-05-06 20:40:40 -0400

    linked lists

--- linked_list.py
+++ linked_list.py
@@ -1,29 +1,47 @@
 class Node:
-	def __init__(self, data=None, next_node=None):
-		self.data = data
-		self.next_node = next_node
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
 class LinkedList:
-	def __init__(self):
-		self.head = None
-		self.last_node = None
+    def __init__(self):
+        self.head = None
+        self.last_node = None
 
-	def print_ll(self):
-		ll_string = ""
-		node = self.head
-		if node is None:
-			print(None)
-		while node is not None:
-			ll_string += f"{str(node.data)} -> "
-			node = node.next_node
-		ll_string += "None"
-		print(ll_string)
+    def print_ll(self):
+        ll_string = ""
+        node = self.head
+        if node is None:
+            print(None)
+        while node is not None:
+            ll_string += f"{str(node.data)} -> "
+            node = node.next
+        ll_string += "None"
+        print(ll_string)
 
-ll = LinkedList()
-node4 = Node("data4", None)
-node3 = Node("data3", node4)
-node2 = Node("data2", node3)
-node1 = Node("data1", node2)
-ll.head = node1
+    def to_array(self):
+        arr = []
+        if self.head is None:
+            return arr
+        
+        node = self.head
+        while node is not None:
+            arr.append(node.data)
+            node = node.next
+        return arr
 
-ll.print_ll()
+    def insert_beginning(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            self.last_node = self.head
+        
+        new_node = Node(data, self.head)
+        self.head = new_node
+
+    # By keeping track of 'last_node', we can insert at end in O(1) time rather than O(n)
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.insert_beginning(data)
+
+        self.last_node.next = Node(data, None)
+        self.last_node = self.last_node.next
