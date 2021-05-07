9ba17eb1ce19abe79fc7131b01591fcd974ba168
ncolonna <colonnan96@gmail.com>
2021-05-06 21:03:02 -0400

    get all users routes

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import linked_list
 
 # app
 app = Flask(__name__)
@@ -14,10 +15,10 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 # configuere sqlite3 to enforce foreign key constraints
 @event.listens_for(Engine, "connect")
 def _set_sqlite_pragma(dbapi_connection, connection_record):
-	if isinstance(dbapi_connection, SQLite3Connection):
-		cursor = dbapi_connection.cursor()
-		cursor.execute("PRAGMA foreign_keys=ON;")
-		cursor.close()
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
 
 db = SQLAlchemy(app)
 now = datetime.now()
@@ -25,67 +26,93 @@ now = datetime.now()
 # models
 # for ORM - class to represent a database table
 class User(db.Model):
-	__tablename__ = "user"
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(50))
-	email = db.Column(db.String(50))
-	address = db.Column(db.String(200))
-	phone = db.Column(db.String(50))
-	posts = db.relationship("BlogPost") 	# relationship to another table "BlogPost"
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost")     # relationship to another table "BlogPost"
 
 class BlogPost(db.Model):
-	__table__name = "blog_post"
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(50))
-	body = db.Column(db.String(200))
-	date = db.Column(db.Date)
-	user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    __table__name = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 # routes
 @app.route("/user", methods=["POST"])
 def create_user():
-	data = request.get_json()
-	new_user = User(
-		name = data["name"],
-		email = data["email"],
-		address = data["address"],
-		phone = data["phone"],
-	)
-	db.session.add(new_user)
-	db.session.commit()
-	return jsonify({"message": "User created"}), 200
+    data = request.get_json()
+    new_user = User(
+        name = data["name"],
+        email = data["email"],
+        address = data["address"],
+        phone = data["phone"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 200
 
 @app.route("/user/descending_id", methods=["GET"])
 def get_all_users_descending():
-	pass
+    users = User.query.all()
+    all_users_ll = linked_list.LinkedList()
+
+    for user in users:
+        all_users_ll.insert_beginning(
+            {
+                "id" : user.id,
+                "name": user.name,
+                "email": user.email,
+                "address": user.address,
+                "phone": user.phone,
+            }
+        )
+    return jsonify(all_users_ll.to_list()), 200
 
 @app.route("/user/ascending_id", methods=["GET"])
 def get_all_users_ascending():
-	pass
+    users = User.query.all()
+    all_users_ll = linked_list.LinkedList()
+
+    for user in users:
+        all_users_ll.insert_at_end(
+            {
+                "id" : user.id,
+                "name": user.name,
+                "email": user.email,
+                "address": user.address,
+                "phone": user.phone,
+            }
+        )
+    return jsonify(all_users_ll.to_list()), 200
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_one_user(user_id):
-	pass
+    pass
 
 @app.route("/user/<user_id>", methods=["DELETE"])
 def delete_user(user_id):
-	pass
+    pass
 
 @app.route("/blog_post/<user_id>", methods=["POST"])
 def create_blog_post(user_id):
-	pass
+    pass
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_all__blog_posts(user_id):
-	pass
+    pass
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_one_blog_post(blog_post_id):
-	pass
+    pass
 
 @app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
 def delete_blog_post(blog_post_id):
-	pass
+    pass
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
